215eac6a899318cf1918717c28ed1ebcbb02b6a0
Ahmad <iahmd01@gmail.com>
2020-09-01 09:12:03 +0300

    an attempt to fix all three issues

--- bigint-optimized/small_obj_storage.h
+++ bigint-optimized/small_obj_storage.h
@@ -10,7 +10,6 @@
 // ***Copy-on-write and small-object optimised storage***
 template<typename T>
 struct small_obj_storage {
-
   static const size_t SMALL_OBJECT_SIZE = sizeof(cow_storage<T>*) / sizeof(T) + 1;
   small_obj_storage() = default;
   small_obj_storage(const small_obj_storage &other);
@@ -28,75 +27,70 @@ struct small_obj_storage {
   T& operator[](size_t id);
   const T& back() const;
 
+private:
   union united_storage {
-    cow_storage<T> *big_storage;
     struct {
-      T buff[SMALL_OBJECT_SIZE] = {0};
+      T buff[SMALL_OBJECT_SIZE];
       size_t len = 0;
-    } small_storage{};
+    } static_storage;
+    cow_storage<T> *dynamic_storage;
 
     T& get_ith(bool is_promoted, size_t i);
-    const T& get_ith(bool is_promoted, size_t i) const;
+    const T& read_ith(bool is_promoted, size_t i) const;
     void promote();
-  } small_obj_buff;
+  } small_obj_buff = {};
 
   bool promoted = false;
 };
 
 template<typename T>
 small_obj_storage<T>::small_obj_storage(const small_obj_storage<T> &other) {
-  if (this == &other) {
-    return;
+  if (other.size() <= SMALL_OBJECT_SIZE) {
+    resize(other.size());
+    for (size_t i = 0; i < other.size(); i++) {
+      (*this)[i] = other[i];
+    }
+  } else {
+    small_obj_buff.promote();
+    promoted = true;
+    *small_obj_buff.dynamic_storage = *other.small_obj_buff.dynamic_storage;
   }
-  *this = other;
 }
 
 template<typename T>
 small_obj_storage<T>& small_obj_storage<T>::operator=(const small_obj_storage<T> &other) {
-  if (this == &other) {
-    return *this;
-  }
-  if (other.size() <= SMALL_OBJECT_SIZE && !promoted) {
-    for (size_t i = 0; i < other.size(); i++) {
-      small_obj_buff.small_storage.buff[i] = other[i];
-    }
-    small_obj_buff.small_storage.len = other.size();
-  } else {
-    if (!promoted) {
-      small_obj_buff.promote();
-      promoted = true;
-    }
-    small_obj_buff.big_storage->resize(other.size());
-    for (size_t i = 0; i < other.size(); i++) {
-      (*small_obj_buff.big_storage)[i] = other[i];
-    }
-  }
+  small_obj_storage<T> temp = other;
+  using std::swap;
+  swap(this->promoted, temp.promoted);
+  swap(this->small_obj_buff, temp.small_obj_buff);
   return *this;
 }
 
 template<typename T>
 size_t small_obj_storage<T>::size() const {
-  return promoted ? small_obj_buff.big_storage->size() : small_obj_buff.small_storage.len;
+  return promoted ?
+  small_obj_buff.dynamic_storage->read_storage().size()
+  : small_obj_buff.static_storage.len;
 }
 
 template<typename T>
 void small_obj_storage<T>::resize(size_t new_sz) {
-  return resize(new_sz, 0);
+  return resize(new_sz, T());
 }
 
 template<typename T>
 void small_obj_storage<T>::resize(size_t new_sz, T fill_value) {
   if (promoted) {
-    small_obj_buff.big_storage->resize(new_sz, fill_value);
+    small_obj_buff.dynamic_storage->get_storage().resize(new_sz, fill_value);
   } else {
     if (new_sz <= SMALL_OBJECT_SIZE) {
       auto how_much = new_sz > size() ? new_sz - size() : 0;
-      std::fill_n(small_obj_buff.small_storage.buff + size(), how_much, fill_value);
-      small_obj_buff.small_storage.len = new_sz;
+      std::fill_n(small_obj_buff.static_storage.buff + size(), how_much, fill_value);
+      small_obj_buff.static_storage.len = new_sz;
     } else {
       small_obj_buff.promote();
       promoted = true;
-      small_obj_buff.big_storage->resize(new_sz, fill_value);
+      small_obj_buff.dynamic_storage->get_storage().resize(new_sz, fill_value);
     }
   }
 }
@@ -113,7 +107,7 @@ void small_obj_storage<T>::clear() {
 
 template<typename T>
 const T& small_obj_storage<T>::operator[](size_t id) const {
-  return small_obj_buff.get_ith(promoted, id);
+  return small_obj_buff.read_ith(promoted, id);
 }
 
 template<typename T>
@@ -129,28 +123,28 @@ const T& small_obj_storage<T>::back() const {
 template<typename T>
 small_obj_storage<T>::~small_obj_storage() {
   if (promoted) {
-    delete small_obj_buff.big_storage;
+    delete small_obj_buff.dynamic_storage;
   }
 }
 
 template<typename T>
 void small_obj_storage<T>::united_storage::promote() {
-  auto *new_buffer = new std::vector<T>(small_storage.len);
-  std::copy(small_storage.buff,
-            small_storage.buff + small_storage.len,
-            new_buffer->begin());
-  big_storage = new cow_storage<T>();
-  big_storage->common_buff.reset(new_buffer);
+  auto new_buffer = std::vector<T>(static_storage.len);
+  std::copy(static_storage.buff,
+            static_storage.buff + static_storage.len,
+            new_buffer.begin());
+  dynamic_storage = new cow_storage<T>();
+  dynamic_storage->get_storage() = new_buffer;
 }
 
 template<typename T>
 T& small_obj_storage<T>::united_storage::get_ith(bool is_promoted, size_t i) {
-  return is_promoted ? (*big_storage)[i] : small_storage.buff[i];
+  return is_promoted ? dynamic_storage->get_storage()[i] : static_storage.buff[i];
 }
 
 template<typename T>
-const T& small_obj_storage<T>::united_storage::get_ith(bool is_promoted, size_t i) const {
-  return is_promoted ? (*big_storage)[i] : small_storage.buff[i];
+const T& small_obj_storage<T>::united_storage::read_ith(bool is_promoted, size_t i) const {
+  return is_promoted ? dynamic_storage->read_storage()[i] : static_storage.buff[i];
 }
 
 #endif //BIGINT_SMALL_OBJ_STORAGE_H
