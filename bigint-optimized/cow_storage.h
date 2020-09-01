a3b351455b5dbfd70da234fe3728f3b9b838cac8
Ahmad <iahmd01@gmail.com>
2020-09-01 09:12:03 +0300

    an attempt to fix all three issues

--- bigint-optimized/cow_storage.h
+++ bigint-optimized/cow_storage.h
@@ -13,94 +13,54 @@
 
 template<typename T>
 struct cow_storage {
-  cow_storage();
-  cow_storage& operator=(const cow_storage &other);
 
-  size_t size() const;
-  void resize(size_t new_sz);
-  void resize(size_t new_sz, T fill_value);
-  void push_back(const T &val);
-  void clear();
-  const T& operator[](size_t id) const;
-  T& operator[](size_t id);
-  const T& back() const;
+  cow_storage() noexcept = default;
+  cow_storage(const cow_storage &other) noexcept;
+  cow_storage& operator=(const cow_storage &other) noexcept;
+  ~cow_storage() noexcept = default;
 
-  std::shared_ptr< std::vector<T> > common_buff;
+  std::vector<T> const& read_storage();
+  std::vector<T>& get_storage();
 
 private:
-  void unify_if_needed();
+  void allocate_buffer_if_empty();
+
+  typename std::enable_if<
+  std::is_fundamental<T>::value && std::is_integral<T>::value,
+  std::shared_ptr< std::vector<T> > >::type common_buff;
 };
 
 template<typename T>
-cow_storage<T>::cow_storage() {
-  common_buff.reset(new std::vector<T>());
-}
+cow_storage<T>::cow_storage(const cow_storage &other) noexcept : common_buff(other.common_buff) {}
 
 template<typename T>
-cow_storage<T> &cow_storage<T>::operator=(
-        const cow_storage &other) {
-  if (this == &other) {
-    return *this;
-  }
-  common_buff = other.common_buff;
+cow_storage<T>& cow_storage<T>::operator=(const cow_storage &other) noexcept {
+  cow_storage temp = other;
+  using std::swap;
+  swap(this->common_buff, temp.common_buff);
   return *this;
 }
 
 template<typename T>
-size_t cow_storage<T>::size() const {
-  return common_buff->size();
-}
-
-template<typename T>
-void cow_storage<T>::resize(size_t new_sz) {
-  if (new_sz != common_buff->size()) {
-    unify_if_needed();
-    common_buff->resize(new_sz);
+void cow_storage<T>::allocate_buffer_if_empty() {
+  if (common_buff.use_count() == 0) {
+    common_buff.reset(new std::vector<T>());
   }
 }
 
 template<typename T>
-void cow_storage<T>::resize(size_t new_sz, T fill_value) {
-  if (new_sz != common_buff->size()) {
-    unify_if_needed();
-    common_buff->resize(new_sz, fill_value);
-  }
+std::vector<T> const& cow_storage<T>::read_storage() {
+  allocate_buffer_if_empty();
+  return *(common_buff.get());
 }
 
 template<typename T>
-void cow_storage<T>::push_back(const T &val) {
-  unify_if_needed();
-  common_buff->push_back(val);
-}
-
-template<typename T>
-void cow_storage<T>::clear() {
-  unify_if_needed();
-  common_buff->clear();
-}
-
-template<typename T>
-const T &cow_storage<T>::operator[](size_t id) const {
-  return (*common_buff)[id];
-}
-
-template<typename T>
-T &cow_storage<T>::operator[](size_t id) {
-  unify_if_needed();
-  return (*common_buff)[id];
-}
-
-template<typename T>
-const T &cow_storage<T>::back() const {
-  return common_buff->back();
-}
-
-template<typename T>
-void cow_storage<T>::unify_if_needed() {
+std::vector<T> &cow_storage<T>::get_storage() {
+  allocate_buffer_if_empty();
   if (!common_buff.unique()) {
-    auto *new_buffer = new std::vector<T>(*common_buff);
-    common_buff.reset(new_buffer);
+    common_buff.reset(new std::vector<T>(*common_buff));
   }
+  return *(common_buff.get());
 }
 
 #endif //BIGINT_COW_STORAGE_H
